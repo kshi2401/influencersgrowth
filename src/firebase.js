874cf63f29c7ec9947312ebe7d685835c89a90c3
Khaild Shire <khaildshire@Khailds-MacBook-Air.local>
2025-07-29 16:28:34 -0500

    Favicon set up

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDVTlckV3-gRxMyLZzVfpiCv4dgmY2UW3g",
-  authDomain: "influencersgrowth.firebaseapp.com",
-  projectId: "influencersgrowth",
-  storageBucket: "influencersgrowth.firebasestorage.app",
-  messagingSenderId: "514440129663",
-  appId: "1:514440129663:web:1668443a03979e8da0a34a",
-  measurementId: "G-JVRND3VE4H"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
